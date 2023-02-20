a5587e9bab85487199ee24d85335779bc69cfabc
fiberkang <fiberkang@126.com>
2023-02-20 16:40:44 +0800

    初次提交

---
+++ test1/src/com/lipeng/unety/entity/User.java
@@ -0,0 +1,45 @@
+package com.lipeng.unety.entity;
+
+public class User {
+    private  int id;
+    private String name;
+    private String pwd;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    public User(String name, String pwd) {
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
